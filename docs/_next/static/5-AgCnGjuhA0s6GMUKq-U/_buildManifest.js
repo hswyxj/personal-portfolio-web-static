78d46b8e0e418daa33aa1265ca704f162d2e7ad2
郝斯文 <h19111426051@163.com>
2026-04-15 11:27:46 +0800

    Add documentation and SVG assets; update package.json for predeploy build step
    - Created index.txt for documentation with various component references.
    - Added SVG files for Next.js, Vercel, and Window icons.
    - Updated package.json to include a predeploy build step for Next.js.

---
+++ docs/_next/static/5-AgCnGjuhA0s6GMUKq-U/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/personal-portfolio-web-static//_next/:path+",
+        "destination": "/personal-portfolio-web-static/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
